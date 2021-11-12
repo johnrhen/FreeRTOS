1d0b410dfc717d2e43258130288388283fd54739
John Hendrickson <johnrhen@amazon.com>
2021-11-12 14:31:38 -0800

    Update formatting on demo_config.h

--- FreeRTOS-Plus/Demo/AWS/Fleet_Provisioning_Windows_Simulator/Fleet_Provisioning_With_CSR_Demo/demo_config.h
+++ FreeRTOS-Plus/Demo/AWS/Fleet_Provisioning_Windows_Simulator/Fleet_Provisioning_With_CSR_Demo/demo_config.h
@@ -23,26 +23,26 @@
 #ifndef DEMO_CONFIG_H_
 #define DEMO_CONFIG_H_
 
- /**************************************************/
- /******* DO NOT CHANGE the following order ********/
- /**************************************************/
+/**************************************************/
+/******* DO NOT CHANGE the following order ********/
+/**************************************************/
 
- /* Include logging header files and define logging macros in the following order:
-  * 1. Include the header file "logging_levels.h".
-  * 2. Define LIBRARY_LOG_NAME and  LIBRARY_LOG_LEVEL.
-  * 3. Include the header file "logging_stack.h".
-  */
+/* Include logging header files and define logging macros in the following order:
+ * 1. Include the header file "logging_levels.h".
+ * 2. Define LIBRARY_LOG_NAME and  LIBRARY_LOG_LEVEL.
+ * 3. Include the header file "logging_stack.h".
+ */
 
-  /* Include header that defines log levels. */
+/* Include header that defines log levels. */
 #include "logging_levels.h"
 
 /* Logging configuration for the Demo. */
 #ifndef LIBRARY_LOG_NAME
-#define LIBRARY_LOG_NAME    "FLEET_PROVISIONING_DEMO"
+    #define LIBRARY_LOG_NAME    "FLEET_PROVISIONING_DEMO"
 #endif
 
 #ifndef LIBRARY_LOG_LEVEL
-#define LIBRARY_LOG_LEVEL    LOG_INFO
+    #define LIBRARY_LOG_LEVEL    LOG_INFO
 #endif
 
 #include "logging_stack.h"
@@ -61,250 +61,250 @@
  */
 #define AWS_IOT_ENDPOINT               "a2ri0cd9rhrf0y-ats.iot.us-west-2.amazonaws.com"
 
- /**
-  * @brief AWS IoT MQTT broker port number.
-  *
-  * In general, port 8883 is for secured MQTT connections.
-  *
-  * @note Port 443 requires use of the ALPN TLS extension with the ALPN protocol
-  * name. When using port 8883, ALPN is not required.
-  */
+/**
+ * @brief AWS IoT MQTT broker port number.
+ *
+ * In general, port 8883 is for secured MQTT connections.
+ *
+ * @note Port 443 requires use of the ALPN TLS extension with the ALPN protocol
+ * name. When using port 8883, ALPN is not required.
+ */
 #define AWS_MQTT_PORT    ( 8883 )
 #define democonfigMQTT_BROKER_PORT ( 8883 ) 
 
-  /**
-   * @brief Path of the file containing the server's root CA certificate.
-   *
-   * This certificate is used to identify the AWS IoT server and is publicly
-   * available. Refer to the AWS documentation available in the link below
-   * https://docs.aws.amazon.com/iot/latest/developerguide/server-authentication.html#server-authentication-certs
-   *
-   * Amazon's root CA certificate is automatically downloaded to the certificates
-   * directory from @ref https://www.amazontrust.com/repository/AmazonRootCA1.pem
-   * using the CMake build system.
-   *
-   * @note This certificate should be PEM-encoded.
-   * @note This path is relative from the demo binary created. Update
-   * ROOT_CA_CERT_PATH to the absolute path if this demo is executed from elsewhere.
-   */
+/**
+ * @brief Path of the file containing the server's root CA certificate.
+ *
+ * This certificate is used to identify the AWS IoT server and is publicly
+ * available. Refer to the AWS documentation available in the link below
+ * https://docs.aws.amazon.com/iot/latest/developerguide/server-authentication.html#server-authentication-certs
+ *
+ * Amazon's root CA certificate is automatically downloaded to the certificates
+ * directory from @ref https://www.amazontrust.com/repository/AmazonRootCA1.pem
+ * using the CMake build system.
+ *
+ * @note This certificate should be PEM-encoded.
+ * @note This path is relative from the demo binary created. Update
+ * ROOT_CA_CERT_PATH to the absolute path if this demo is executed from elsewhere.
+ */
 #ifndef ROOT_CA_CERT_PATH
-#define ROOT_CA_CERT_PATH    "C:/Users/johnrhen/certificates/AmazonRootCA1.crt"
+    #define ROOT_CA_CERT_PATH    "C:/Users/johnrhen/certificates/AmazonRootCA1.crt"
 #endif
 
-   /**
-    * @brief Path of the file containing the provisioning claim certificate. This
-    * certificate is used to connect to AWS IoT Core and use Fleet Provisioning
-    * APIs to provision the client device. This is used for the "Provisioning by
-    * Claim" provisioning workflow.
-    *
-    * For information about provisioning by claim, see the following AWS documentation:
-    * https://docs.aws.amazon.com/iot/latest/developerguide/provision-wo-cert.html#claim-based
-    *
-    * @note This certificate should be PEM-encoded. The certificate should be
-    * registered on AWS IoT Core beforehand. It should have an AWS IoT policy to
-    * allow it to access only the Fleet Provisioning APIs. An example policy for
-    * the claim certificates for this demo is available in the
-    * example_claim_policy.json file in the demo directory. In the example,
-    * replace <aws-region> with your AWS region, <aws-account-id> with your
-    * account ID, and <template-name> with the name of your provisioning template.
-    *
-    * #define CLAIM_CERT_PATH    "...insert here..."
-    */
+/**
+ * @brief Path of the file containing the provisioning claim certificate. This
+ * certificate is used to connect to AWS IoT Core and use Fleet Provisioning
+ * APIs to provision the client device. This is used for the "Provisioning by
+ * Claim" provisioning workflow.
+ *
+ * For information about provisioning by claim, see the following AWS documentation:
+ * https://docs.aws.amazon.com/iot/latest/developerguide/provision-wo-cert.html#claim-based
+ *
+ * @note This certificate should be PEM-encoded. The certificate should be
+ * registered on AWS IoT Core beforehand. It should have an AWS IoT policy to
+ * allow it to access only the Fleet Provisioning APIs. An example policy for
+ * the claim certificates for this demo is available in the
+ * example_claim_policy.json file in the demo directory. In the example,
+ * replace <aws-region> with your AWS region, <aws-account-id> with your
+ * account ID, and <template-name> with the name of your provisioning template.
+ *
+ * #define CLAIM_CERT_PATH    "...insert here..."
+ */
 #define CLAIM_CERT_PATH    "C:/Users/johnrhen/certificates/device.pem.crt"
 
-    /**
-     * @brief Path of the file containing the provisioning claim private key. This
-     * key corresponds to the provisioning claim certificate and is used to
-     * authenticate with AWS IoT for provisioning by claim.
-     *
-     * For information about provisioning by claim, see the following AWS documentation:
-     * https://docs.aws.amazon.com/iot/latest/developerguide/provision-wo-cert.html#claim-based
-     *
-     * @note This private key should be PEM-encoded.
-     *
-     * #define CLAIM_PRIVATE_KEY_PATH    "...insert here..."
-     */
+/**
+ * @brief Path of the file containing the provisioning claim private key. This
+ * key corresponds to the provisioning claim certificate and is used to
+ * authenticate with AWS IoT for provisioning by claim.
+ *
+ * For information about provisioning by claim, see the following AWS documentation:
+ * https://docs.aws.amazon.com/iot/latest/developerguide/provision-wo-cert.html#claim-based
+ *
+ * @note This private key should be PEM-encoded.
+ *
+ * #define CLAIM_PRIVATE_KEY_PATH    "...insert here..."
+ */
 #define CLAIM_PRIVATE_KEY_PATH    "C:/Users/johnrhen/certificates/private.pem.key"
 
-     /**
-      * @brief Name of the provisioning template to use for the RegisterThing
-      * portion of the Fleet Provisioning workflow.
-      *
-      * For information about provisioning templates, see the following AWS documentation:
-      * https://docs.aws.amazon.com/iot/latest/developerguide/provision-template.html#fleet-provision-template
-      *
-      * The example template used for this demo is available in the
-      * example_demo_template.json file in the demo directory. In the example,
-      * replace <provisioned-thing-policy> with the policy provisioned devices
-      * should have.  The demo template uses Fn::Join to construct the Thing name by
-      * concatenating fp_demo_ and the serial number sent by the demo.
-      *
-      * @note The provisioning template MUST be created in AWS IoT before running the
-      * demo.
-      *
-      * #define PROVISIONING_TEMPLATE_NAME    "...insert here..."
-      */
+/**
+ * @brief Name of the provisioning template to use for the RegisterThing
+ * portion of the Fleet Provisioning workflow.
+ *
+ * For information about provisioning templates, see the following AWS documentation:
+ * https://docs.aws.amazon.com/iot/latest/developerguide/provision-template.html#fleet-provision-template
+ *
+ * The example template used for this demo is available in the
+ * example_demo_template.json file in the demo directory. In the example,
+ * replace <provisioned-thing-policy> with the policy provisioned devices
+ * should have.  The demo template uses Fn::Join to construct the Thing name by
+ * concatenating fp_demo_ and the serial number sent by the demo.
+ *
+ * @note The provisioning template MUST be created in AWS IoT before running the
+ * demo.
+ *
+ * #define PROVISIONING_TEMPLATE_NAME    "...insert here..."
+ */
 #define PROVISIONING_TEMPLATE_NAME    "FPDemoTemplate"
 
-      /**
-       * @brief Serial number to send in the request to the Fleet Provisioning
-       * RegisterThing API.
-       *
-       * This is sent as a parameter to the provisioning template, which uses it to
-       * generate a unique Thing name. This should be unique per device.
-       *
-       * #define DEVICE_SERIAL_NUMBER    "...insert here..."
-       */
+/**
+ * @brief Serial number to send in the request to the Fleet Provisioning
+ * RegisterThing API.
+ *
+ * This is sent as a parameter to the provisioning template, which uses it to
+ * generate a unique Thing name. This should be unique per device.
+ *
+ * #define DEVICE_SERIAL_NUMBER    "...insert here..."
+ */
 #define DEVICE_SERIAL_NUMBER    "12345"
 
-       /**
-        * @brief Subject name to use when creating the certificate signing request (CSR)
-        * for provisioning the demo client with using the Fleet Provisioning
-        * CreateCertificateFromCsr APIs.
-        *
-        * This is passed to MbedTLS; see https://tls.mbed.org/api/x509__csr_8h.html#a954eae166b125cea2115b7db8c896e90
-        */
+/**
+ * @brief Subject name to use when creating the certificate signing request (CSR)
+ * for provisioning the demo client with using the Fleet Provisioning
+ * CreateCertificateFromCsr APIs.
+ *
+ * This is passed to MbedTLS; see https://tls.mbed.org/api/x509__csr_8h.html#a954eae166b125cea2115b7db8c896e90
+ */
 #ifndef CSR_SUBJECT_NAME
-#define CSR_SUBJECT_NAME    "CN=Fleet Provisioning Demo"
+    #define CSR_SUBJECT_NAME    "CN=Fleet Provisioning Demo"
 #endif
 
-        /**
-         * @brief MQTT client identifier.
-         *
-         * No two clients may use the same client identifier simultaneously.
-         *
-         * @note The client identifier should match the Thing name per
-         * AWS IoT Security best practices:
-         * https://docs.aws.amazon.com/iot/latest/developerguide/security-best-practices.html
-         * However, it is not required for the demo to run.
-         */
+/**
+ * @brief MQTT client identifier.
+ *
+ * No two clients may use the same client identifier simultaneously.
+ *
+ * @note The client identifier should match the Thing name per
+ * AWS IoT Security best practices:
+ * https://docs.aws.amazon.com/iot/latest/developerguide/security-best-practices.html
+ * However, it is not required for the demo to run.
+ */
 #ifndef CLIENT_IDENTIFIER
-#define CLIENT_IDENTIFIER    DEVICE_SERIAL_NUMBER
+    #define CLIENT_IDENTIFIER    DEVICE_SERIAL_NUMBER
 #define democonfigCLIENT_IDENTIFIER DEVICE_SERIAL_NUMBER
 #endif
 
-         /**
-          * @brief Size of the network buffer for MQTT packets. Must be large enough to
-          * hold the GetCertificateFromCsr response, which, among other things, includes
-          * a PEM encoded certificate.
-          */
+/**
+ * @brief Size of the network buffer for MQTT packets. Must be large enough to
+ * hold the GetCertificateFromCsr response, which, among other things, includes
+ * a PEM encoded certificate.
+ */
 #define NETWORK_BUFFER_SIZE       ( 2048U )
 
-          /**
-           * @brief The name of the operating system that the application is running on.
-           * The current value is given as an example. Please update for your specific
-           * operating system.
-           */
+/**
+ * @brief The name of the operating system that the application is running on.
+ * The current value is given as an example. Please update for your specific
+ * operating system.
+ */
 #define OS_NAME                   "Ubuntu"
 
-           /**
-            * @brief The version of the operating system that the application is running
-            * on. The current value is given as an example. Please update for your specific
-            * operating system version.
-            */
+/**
+ * @brief The version of the operating system that the application is running
+ * on. The current value is given as an example. Please update for your specific
+ * operating system version.
+ */
 #define OS_VERSION                "18.04 LTS"
 
-            /**
-             * @brief The name of the hardware platform the application is running on. The
-             * current value is given as an example. Please update for your specific
-             * hardware platform.
-             */
+/**
+ * @brief The name of the hardware platform the application is running on. The
+ * current value is given as an example. Please update for your specific
+ * hardware platform.
+ */
 #define HARDWARE_PLATFORM_NAME    "PC"
 
-             /**
-              * @brief The name of the MQTT library used and its version, following an "@"
-              * symbol.
-              */
+/**
+ * @brief The name of the MQTT library used and its version, following an "@"
+ * symbol.
+ */
 #include "core_mqtt.h"
 #define MQTT_LIB    "core-mqtt@" MQTT_LIBRARY_VERSION
 
-              /**
-               * @brief The name of the operating system that the application is running on.
-               * The current value is given as an example. Please update for your specific
-               * operating system.
-               */
+/**
+ * @brief The name of the operating system that the application is running on.
+ * The current value is given as an example. Please update for your specific
+ * operating system.
+ */
 #define democonfigOS_NAME                   "FreeRTOS"
 
-               /**
-                * @brief The version of the operating system that the application is running
-                * on. The current value is given as an example. Please update for your specific
-                * operating system version.
-                */
+/**
+ * @brief The version of the operating system that the application is running
+ * on. The current value is given as an example. Please update for your specific
+ * operating system version.
+ */
 #define democonfigOS_VERSION                tskKERNEL_VERSION_NUMBER
 
-                /**
-                 * @brief The name of the hardware platform the application is running on. The
-                 * current value is given as an example. Please update for your specific
-                 * hardware platform.
-                 */
+/**
+ * @brief The name of the hardware platform the application is running on. The
+ * current value is given as an example. Please update for your specific
+ * hardware platform.
+ */
 #define democonfigHARDWARE_PLATFORM_NAME    "WinSim"
 
-                 /**
-                  * @brief The name of the MQTT library used and its version, following an "@"
-                  * symbol.
-                  */
+/**
+ * @brief The name of the MQTT library used and its version, following an "@"
+ * symbol.
+ */
 #include "core_mqtt.h" /* Include coreMQTT header for MQTT_LIBRARY_VERSION macro. */
 #define democonfigMQTT_LIB                               "core-mqtt@"MQTT_LIBRARY_VERSION
 
-                  /**
-                   * @brief Set the stack size of the main demo task.
-                   *
-                   * In the Windows port, this stack only holds a structure. The actual
-                   * stack is created by an operating system thread.
-                   */
+/**
+ * @brief Set the stack size of the main demo task.
+ *
+ * In the Windows port, this stack only holds a structure. The actual
+ * stack is created by an operating system thread.
+ */
 #define democonfigDEMO_STACKSIZE                         configMINIMAL_STACK_SIZE
 
-                   /**
-                    * @brief Size of the network buffer for MQTT packets.
-                    */
+/**
+ * @brief Size of the network buffer for MQTT packets.
+ */
 #define democonfigNETWORK_BUFFER_SIZE                    ( 1024U )
 
-                    /**
-                     * @brief Size of the open TCP ports array.
-                     *
-                     * A maximum of these many open TCP ports will be sent in the device defender
-                     * report.
-                     */
+/**
+ * @brief Size of the open TCP ports array.
+ *
+ * A maximum of these many open TCP ports will be sent in the device defender
+ * report.
+ */
 #define democonfigOPEN_TCP_PORTS_ARRAY_SIZE              10
 
-                     /**
-                      * @brief Size of the open UDP ports array.
-                      *
-                      * A maximum of these many open UDP ports will be sent in the device defender
-                      * report.
-                      */
+/**
+ * @brief Size of the open UDP ports array.
+ *
+ * A maximum of these many open UDP ports will be sent in the device defender
+ * report.
+ */
 #define democonfigOPEN_UDP_PORTS_ARRAY_SIZE              10
 
-                      /**
-                       * @brief Size of the established connections array.
-                       *
-                       * A maximum of these many established connections will be sent in the device
-                       * defender report.
-                       */
+/**
+ * @brief Size of the established connections array.
+ *
+ * A maximum of these many established connections will be sent in the device
+ * defender report.
+ */
 #define democonfigESTABLISHED_CONNECTIONS_ARRAY_SIZE     10
 
-                       /**
-                        * @brief Size of the task numbers array.
-                        *
-                        * This must be at least the number of tasks used.
-                        */
+/**
+ * @brief Size of the task numbers array.
+ *
+ * This must be at least the number of tasks used.
+ */
 #define democonfigCUSTOM_METRICS_TASKS_ARRAY_SIZE        10
 
-                        /**
-                         * @brief Size of the buffer which contains the generated device defender report.
-                         *
-                         * If the generated report is larger than this, it is rejected.
-                         */
+/**
+ * @brief Size of the buffer which contains the generated device defender report.
+ *
+ * If the generated report is larger than this, it is rejected.
+ */
 #define democonfigDEVICE_METRICS_REPORT_BUFFER_SIZE      1000
 
-                         /**
-                          * @brief Major version number of the device defender report.
-                          */
+/**
+ * @brief Major version number of the device defender report.
+ */
 #define democonfigDEVICE_METRICS_REPORT_MAJOR_VERSION    1
 
-                          /**
-                           * @brief Minor version number of the device defender report.
-                           */
+/**
+ * @brief Minor version number of the device defender report.
+ */
 #define democonfigDEVICE_METRICS_REPORT_MINOR_VERSION    0
 
 #define democonfigROOT_CA_PEM "C:/Users/johnrhen/certificates/AmazonRootCA1.crt"
